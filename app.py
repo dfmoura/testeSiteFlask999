e3aa2b817b6162bd2c8c9b9416fa27b8741e15ef
dfmoura <diogomou@gmail.com>
2023-04-18 20:59:05 -0300

    novas verificacoes

--- app.py
+++ app.py
@@ -11,5 +11,21 @@ def mostrar():
     return render_template("index.html", msg=mensagem)
 
 
+
+@app.route("/dolarptax", methods=["POST"])
+def dolarptax():
+    ptax = "Ainda não!!!requests.args.get(usdbrl)"
+    return render_template("index.html", ptax=ptax)
+
+@app.route("/dolaratual1", methods=["POST"])
+def valorUSD_BRL():
+    requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+    dic_requisicao = requisicao.json()
+    dolaratual = dic_requisicao['USDBRL']['bid']
+    return render_template("index.html", dolaratual=dolaratual)
+
+
+
+
 if __name__ == '__main__':
     app.run()
